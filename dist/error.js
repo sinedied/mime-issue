0f307307c1cb9e84bd913a66a0291af6c38e1588
sinedied <noda@free.fr>
2024-01-24 10:13:31 +0100

    initial commit

---
+++ dist/error.js
@@ -0,0 +1,5 @@
+import mime from 'mime/lite';
+import mime2 from 'mime/lite.js';
+console.log(mime.getType('txt'));
+console.log(mime2.getType('txt'));
+//# sourceMappingURL=error.js.map
---
+++ error.ts
@@ -0,0 +1,5 @@
+import mime from 'mime/lite';
+import mime2 from 'mime/lite.js';
+
+console.log(mime.getType('txt')); // text/plain
+console.log(mime2.getType('txt')); // text/plain
